109c8d895afe64c86f7fb603a8417f295ba969e7
jordan <jordan.lesich@gmail.com>
2022-02-20 12:17:07 -0800

    introduce styled components

--- src/templates/Posts.jsx
+++ src/templates/Posts.jsx
@@ -1,18 +1,44 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+
+const PageLayout = styled.main`
+  color: #232129;
+  padding: 6rem;
+  font-family: '-apple-system, Roboto, sans-serif, serif';
+  .post-link {
+    color: #727bd1;
+    font-weight: bold;
+    font-size: 20px;
+    /* vertical-align: 5%; */
+  }
+  .list-item {
+    font-weight: 300px;
+    max-width: 560px;
+    margin-bottom: 30px;
+  }
+`;
 
 const Posts = ({ pageContext }) => {
   const { posts } = pageContext;
- 
+
   return (
-    <div>
+    <PageLayout>
       <ul>
         {posts?.map((post) => (
-          <li key={post.key}><Link to={`/post/${post.id}`}>{post.title}</Link></li>
+          <li className='list-item' key={post.id}>
+            <Link className='post-link' to={`/post/${post.id}`}>
+              {post.title}
+            </Link>
+          </li>
         ))}
       </ul>
-    </div>
+    </PageLayout>
   );
 };
 
 export default Posts;
+
+const PostsListing = () => {};
+
+const PostListItem = () => {};
