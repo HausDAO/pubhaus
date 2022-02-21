636558adc62eaf272e070d9df7cfe048f0922624
jordan <jordan.lesich@gmail.com>
2022-02-20 22:34:04 -0800

    add date/styles

--- src/templates/Posts.jsx
+++ src/templates/Posts.jsx
@@ -1,10 +1,11 @@
+import { formatDistanceToNow } from 'date-fns';
 import { Link } from 'gatsby';
 import React from 'react';
 import styled from 'styled-components';
 
 const PageLayout = styled.main`
   color: #232129;
-  padding: 6rem;
+  padding: 4rem;
   font-family: '-apple-system, Roboto, sans-serif, serif';
   .post-link {
     color: #727bd1;
@@ -19,17 +20,38 @@ const PageLayout = styled.main`
   }
 `;
 
+const TitleBox = styled.div`
+  margin-bottom: 4rem;
+  h1 {
+    font-size: 2.8rem;
+  }
+  .sub-title {
+    font-size: 1.6rem;
+    color: #663399;
+  }
+`;
+
+const StyledListItem = styled.li`
+  p {
+    font-size: 1.2rem;
+  }
+  .date-text {
+    font-style: italic;
+  }
+`;
+
 const Posts = ({ pageContext }) => {
   const { posts } = pageContext;
+  console.log('posts', posts);
   return (
     <PageLayout>
+      <TitleBox>
+        <h1>Rage Reports 🔬🧪🔬 </h1>
+        <h2 className='sub-title'>R&D, Hacks, and Rage Driven Development</h2>
+      </TitleBox>
       <ul>
         {posts?.map((post) => (
-          <li className='list-item' key={post.id}>
-            <Link className='post-link' to={`/post/${post.id}`}>
-              {post.title}
-            </Link>
-          </li>
+          <PostListItem key={post.id} post={post} />
         ))}
       </ul>
     </PageLayout>
@@ -38,6 +60,21 @@ const Posts = ({ pageContext }) => {
 
 export default Posts;
 
-const PostsListing = () => {};
-
-const PostListItem = () => {};
+const PostListItem = ({ post }) => (
+  <StyledListItem className='list-item' key={post.id}>
+    <Link className='post-link' to={`/post/${post.id}`}>
+      {post.title}
+    </Link>
+    <p>
+      Description goes here once we add in the graph field. Should there be a
+      metadata field where DAOs can store things like this, or should we enforce
+      descriptions.{' '}
+    </p>
+    <span className='date-text'>
+      Posted:{' '}
+      {formatDistanceToNow(new Date(post.createdAt * 1000), {
+        addSuffix: true,
+      })}
+    </span>
+  </StyledListItem>
+);
